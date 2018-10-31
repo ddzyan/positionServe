4c58c3902e19475ceed775eccd4eb0d678b61fd4
dingdz <ddzyan@163.com>
2018-10-31 19:09:58 +0800

    添加计时器刷新坐标

--- public/javascript/position.js
+++ public/javascript/position.js
@@ -1,83 +1,88 @@
-var markerArr = [{
+var markerArr = [
+  {
     latitude: "120.792084",
     longitude: "30.740111"
-},
-{
+  },
+  {
     latitude: "120.832257",
-    longitude: "30.741601"
-}];
+    longitude: "30.850601"
+  }
+];
 
-var map; //Map实例    
+var map; //Map实例
 function map_init() {
-    map = new BMap.Map("map");
-    //设置中心点，嘉兴市    
-    var point = new BMap.Point(120.762189, 30.75265);
-    //初始化地图    
-    map.centerAndZoom(point, 13);
-    //启用滚轮   
-    map.enableScrollWheelZoom(true);
-    //添加缩放控件    
-    var ctrlNav = new window.BMap.NavigationControl({
-        anchor: BMAP_ANCHOR_TOP_LEFT,
-        type: BMAP_NAVIGATION_CONTROL_LARGE
-    });
-    map.addControl(ctrlNav);
-    //添加比例尺控件    
-    var ctrlSca = new window.BMap.ScaleControl({
-        anchor: BMAP_ANCHOR_BOTTOM_LEFT
-    });
-    map.addControl(ctrlSca);
+  map = new BMap.Map("map");
+  //设置中心点，嘉兴市
+  var point = new BMap.Point(120.762189, 30.75265);
+  //初始化地图
+  map.centerAndZoom(point, 13);
+  //启用滚轮
+  map.enableScrollWheelZoom(true);
+  //添加缩放控件
+  var ctrlNav = new window.BMap.NavigationControl({
+    anchor: BMAP_ANCHOR_TOP_LEFT,
+    type: BMAP_NAVIGATION_CONTROL_LARGE
+  });
+  map.addControl(ctrlNav);
+  //添加比例尺控件
+  var ctrlSca = new window.BMap.ScaleControl({
+    anchor: BMAP_ANCHOR_BOTTOM_LEFT
+  });
+  map.addControl(ctrlSca);
 
-    ajax_get();
+  ajax_get();
+  setInterval(ajax_get, 2000);
 }
 
-// 添加标注    
+// 添加标注
 function addMarker(point, index) {
-    var myIcon = new BMap.Icon("http://api.map.baidu.com/img/markers.png",
-        new BMap.Size(23, 25), {
-            offset: new BMap.Size(10, 25),
-            imageOffset: new BMap.Size(0, 0 - index * 25)
-        });
-    var marker = new BMap.Marker(point, {
-        icon: myIcon
-    });
-    map.addOverlay(marker);
-    return marker;
+  var myIcon = new BMap.Icon(
+    "http://api.map.baidu.com/img/markers.png",
+    new BMap.Size(23, 25),
+    {
+      offset: new BMap.Size(10, 25),
+      imageOffset: new BMap.Size(0, 0 - index * 25)
+    }
+  );
+  var marker = new BMap.Marker(point, {
+    icon: myIcon
+  });
+  map.addOverlay(marker);
+  map.panTo(new_point);
 }
 
 function ajax_get() {
-    $.ajax({
-        type: 'GET',
-        url: 'http://111.231.215.55:3000/positions',
-        dataType: 'json',
-        success: function (msg) {
-            console.log(msg.data);
-            for (let i = 0; i < msg.data.length; i++) {
-                const { latitude: longitude, longitude: latitude } = msg.data[i];
-                console.log(`latitude :${latitude}`);
-                console.log(`longitude :${longitude}`);
-                markerArr.push({
-                    latitude: `${latitude}`,
-                    longitude: `${longitude}`
-                })
-            }
-            for (var i = 0; i < markerArr.length; i++) {
-                console.log(markerArr[i]);
-                addMarker(new window.BMap.Point(markerArr[i].latitude, markerArr[i].longitude), i);
-                //addInfoWindow(maker, markerArr[i], i);     
-            }
-            console.log(markerArr);
-        },
-        error: function (error) {
-            console.log(error);
-        }
-
-    });
+  $.ajax({
+    type: "GET",
+    url: "http://111.231.215.55:3000/positions",
+    dataType: "json",
+    success: function(msg) {
+      map.clearOverlays();
+      for (let i = 0; i < msg.data.length; i++) {
+        const { latitude: longitude, longitude: latitude } = msg.data[i];
+        markerArr.push({
+          latitude: `${latitude}`,
+          longitude: `${longitude}`
+        });
+      }
+      for (var i = 0; i < markerArr.length; i++) {
+        addMarker(
+          new window.BMap.Point(markerArr[i].latitude, markerArr[i].longitude),
+          i
+        );
+      }
+    },
+    error: function(error) {
+      console.log(error);
+    }
+  });
 }
 
 function map_load() {
-    var load = document.createElement("script");
-    load.src = "http://api.map.baidu.com/api?v=1.4&callback=map_init";
-    document.body.appendChild(load);
+  var load = document.createElement("script");
+  load.src =
+    "http://api.map.baidu.com/api?v=2.0&ak=fhfsGtQVwTxwkCw4iA3QHsElDii3P8RE&callback=map_init";
+  document.body.appendChild(load);
 }
+
 window.onload = map_load;
